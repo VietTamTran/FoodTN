3b7e4358403d33b6573bbe5ed15fb92e84067ba5
VietTamTran <55015255+VietTamTran@users.noreply.github.com>
2019-09-26 16:49:32 +0700

    Update model.php

--- model/model.php
+++ model/model.php
@@ -6,7 +6,23 @@
 		//Hàm kết nối.
 		public function __construct()
 		{
-			$this->conn = new mysqli("localhost", "root", "", "foody_thainguyen") or die("Loi ket noi");
+			$connectstr_dbhost = '';
+			$connectstr_dbname = '';
+			$connectstr_dbusername = '';
+			$connectstr_dbpassword = '';
+ 
+			foreach ($_SERVER as $key => $value) {
+			    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+				continue;
+			    }
+    
+			    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+			    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+			    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+			    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+			}
+			
+			$this->conn = new mysqli($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname) or die("Loi ket noi");
 			$this->conn->set_charset("UTF8");
 		}
 
