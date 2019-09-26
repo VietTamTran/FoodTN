77a53237ebc1175fabbe48cd159faff48393eff6
VietTamTran <55015255+VietTamTran@users.noreply.github.com>
2019-09-26 16:55:59 +0700

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -8,16 +8,16 @@
 					<div class="col l4 offset-l2 s12">
 						<h5 class="black-text">Liên hệ:</h5>
 						<ul>
-							<li><a class="black-text text-lighten-3" href="http://fb.com/vanhuy174"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">feedback</i>vanhuy</a></li>
-							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">phone_in_talk</i>0335 147 577</a></li>
-							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">account_circle</i>vanhuy30305@gmail.com</a></li>
+							<li><a class="black-text text-lighten-3" href="http://fb.com/trantam910.dev"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">feedback</i>Viết Tâm Trần</a></li>
+							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">phone_in_talk</i>0963230121</a></li>
+							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">account_circle</i>trantam910.dev@gmail.com</a></li>
 						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="footer-copyright">
 				<div class="container">
-					© 2018 Copy by vanhuy
+					© 2018 Copy by Viết Tâm Trần
 					
 				</div>
 			</div>
