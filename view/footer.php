2ddc2b2fb9cf7da3c904e1471ce2f523ce84ac7d
- <->
2019-09-26 16:37:07 +0700

    foodTN

---
+++ view/footer.php
@@ -0,0 +1,24 @@
+
+			<div class="container" >
+				<div class="row">
+					<div class="col l6 s12">
+						<h5 class="black-text">Food & Drink</h5>
+						<p class="black-text text-lighten-4">Sản phẩm chưa được hoàn thiện. Rất mong mọi người đóng góp ý kiến!</p>
+					</div>
+					<div class="col l4 offset-l2 s12">
+						<h5 class="black-text">Liên hệ:</h5>
+						<ul>
+							<li><a class="black-text text-lighten-3" href="http://fb.com/vanhuy174"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">feedback</i>vanhuy</a></li>
+							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">phone_in_talk</i>0335 147 577</a></li>
+							<li><a class="black-text text-lighten-3" href="#!"><i style="margin-right: 5px; font-size: 15px;" class="material-icons">account_circle</i>vanhuy30305@gmail.com</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright">
+				<div class="container">
+					© 2018 Copy by vanhuy
+					
+				</div>
+			</div>
+			
